105b8d0c0ed2e812d7030852415177714fb3480c
bibilov <bibilov@yandex.ru>
2016-09-19 14:33:27 +0300

    Update friends.py

--- friends.py
+++ friends.py
@@ -9,11 +9,15 @@ data = urlopen('http://api.vk.com/method/friends.get?user_id=270619647').read().
 data = loads(data)
 friends_id = data['response']
 
+
 for f_id in friends_id:
     data = urlopen('http://api.vk.com/method/friends.get?user_id=' + str(f_id)).read().decode('utf8')
     data = loads(data)
-    friends_friends_id = data['response']
-    friends.append([f_id, set(friends_id) & set(friends_friends_id)])
+    try:
+        friends_friends_id = data['response']
+    except:
+        continue
+    friends.append([f_id, len(set(friends_id) & set(friends_friends_id))])
     sleep(0.3)
     print(".", end="")
     
